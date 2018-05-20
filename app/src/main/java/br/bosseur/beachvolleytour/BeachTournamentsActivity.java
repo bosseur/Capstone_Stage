e2430f63d24aca12be0a8bef24da9e2b2fabecff
Ernst Koetsier <ernst.koetsier@sicpa.com>
2018-05-20 19:16:11 -0300

    Initial project commit

---
+++ app/src/main/java/br/bosseur/beachvolleytour/BeachTournamentsActivity.java
@@ -0,0 +1,13 @@
+package br.bosseur.beachvolleytour;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class BeachTournamentsActivity extends AppCompatActivity {
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_beach_tournaments);
+  }
+}
